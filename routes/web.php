7ed9aa64a4f184c68924368146c5aac9eb1f5895
hanalatifa <nasyrahlatifa8.com>
2026-04-17 13:17:44 +0700

    finishing update atlet request

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,39 @@ use App\Http\Controllers\AtletController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProfileController::class, 'index'])->name('welcome');
+// Halaman Publik (HOME)
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function() {
+Route::get('/athletes', function () {
+    return view('athletes-page.athletes');
+})->name('athletes');
 
-    // Manage Atlet 
-    Route::controller(AtletController::class)->group(function() {
-        // Menampilkan Form
-        Route::get('/tambah/atlet', 'create')->name('atlet.create'); 
-        
-        // Memproses Simpan
+Route::get('/achievements', function () {
+    return view('achievements.achievements');
+})->name('achievements');
+
+
+// Halaman yang butuh Login
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Manage Atlet
+    Route::controller(AtletController::class)->group(function () {
+        Route::get('/tambah/atlet', 'create')->name('atlet.create');
         Route::post('/simpan/atlet', 'store')->name('atlet.store');
     });
 
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
